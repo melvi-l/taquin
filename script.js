859b2588b8835d084cdf42a5e7382d71a09103f0
Melvi-l <melvil.miglio@gmail.com>
2025-11-13 19:25:21 +0100

    idk anymore

--- script.js
+++ script.js
@@ -3,4 +3,4 @@ import Experience from "./experience/Experience";
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
-const experience = new Experience(canvas);
+new Experience(canvas);
